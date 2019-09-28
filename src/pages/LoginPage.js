7e61803649faaf763526add2768d4b9683d1c639
Emma Jones <>
2019-09-28 11:27:56 -0400

    mostly css stuff

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,18 +6,21 @@ return(
 <div class="login">
   
     <div class="username">
-    <label>Username </label>
+    <label>Email </label>
     <input type="text" name="username"/>
+
 </div>
 
 <div class="password">
-  <label>Password</label>
+  <label>Password </label>
     <input type="text" name="username"/>
 </div>
 
 <button class="login-button"> 
     login
 </button>
+
+    Don't have an account? Sign up.
 
 </div>
 )
